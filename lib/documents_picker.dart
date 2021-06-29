5848253ee516d6674efb0efc114b4b563e61ed88
Fernando Éricles <feericles@gmail.com>
2021-06-29 16:42:17 -0300

    null safety

--- lib/documents_picker.dart
+++ lib/documents_picker.dart
@@ -6,8 +6,8 @@ class DocumentsPicker {
   static const MethodChannel _channel =
       const MethodChannel('documents_picker');
 
-  static Future<List<String>> get pickDocuments async {
-    final List<dynamic> docsPaths = await _channel.invokeMethod('pickDocuments');
+  static Future<List<String>?> get pickDocuments async {
+    final List<dynamic>? docsPaths = await _channel.invokeMethod('pickDocuments');
     if (docsPaths != null && docsPaths.isEmpty) return [];
     return docsPaths?.cast<String>();
   }
